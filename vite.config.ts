1d70b6b50406cc2ded5c8c8273680a4d4fb1626e
gg122ma <greencucumbertube@gmail.com>
2026-06-28 11:47:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,8 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 添加 base 配置，设置为 './' 可以完美适配 GitHub Pages 的二级目录
+  base: "./", 
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
